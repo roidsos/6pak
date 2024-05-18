831630848bd8405a914ac5e14f5b557a264d79c1
lolguy91 <retek1544@gmail.com>
2024-05-18 09:40:43 +0000

    repo parsing

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
+const r = @import("repo.zig");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    try stdout.print("Hello Assss!\n", .{});
+    const repo = try r.init("testrepo.json");
+    try stdout.print("name: {s}\n", .{repo.name});
+    try stdout.print("description: {s}\n", .{repo.description});
+    for (repo.urls) |url| {
+        try stdout.print("url: {s}\n", .{url});
+    }
 }
---
+++ src/repo.zig
@@ -0,0 +1,19 @@
+const std =  @import("std");
+
+pub const Repo = struct{
+    name: []const u8,
+    description: []const u8,
+    version: []const u8,
+    urls: []const []const u8,
+};
+
+pub fn init(path: []const u8) !Repo {
+    var file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+    const file_buf: []u8 = try file.readToEndAlloc(std.heap.page_allocator, 1024 * 1024); 
+    return (std.json.parseFromSlice( Repo,std.heap.page_allocator,file_buf,.{}) catch |err| {
+        std.debug.print("Error parsing JSON: {s}\n", .{@errorName(err)});
+        std.process.exit(0xff);
+        return err; // wont get here anyways lul
+    }).value;
+}
