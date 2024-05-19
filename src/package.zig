96781e9d0faa5384d101fde2474d417d5c259aec
lolguy91 <retek1544@gmail.com>
2024-05-19 09:39:55 +0000

    made downloading the package.json work

--- src/package.zig
+++ src/package.zig
@@ -1,22 +1,28 @@
 const std = @import("std");
 
-// becuz im lazy
-const http = std.http;
-
 pub fn install(url: []const u8) !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print(" - Downloading \"{s}\"\n", .{url});
+    try stdout.print("Downloading the package...\n", .{});
 
     const allocator = std.heap.page_allocator;
     const buffer = try getBuffFromUrl(allocator, url);
     defer allocator.free(buffer);
-    std.debug.print("{any}\n", .{buffer});
+    std.debug.print("{s}\n", .{buffer});
 }
 
 pub fn getBuffFromUrl(allocator: std.mem.Allocator, url: []const u8) ![]u8 {
-    _ = allocator;
-    _ = url;
+    const uri = std.Uri.parse(url) catch unreachable;
 
-    const buffer: []u8 = undefined;
-    return buffer;
+    var client = std.http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    var request = try client.open(.GET, uri, .{ .server_header_buffer = try allocator.alloc(u8, 1024) });
+    defer request.deinit();
+
+    try request.send();
+    try request.wait();
+
+    const body = request.reader().readAllAlloc(allocator, 8192) catch unreachable;
+
+    return body;
 }
