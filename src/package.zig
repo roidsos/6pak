54908daaccae8ee2d18e298a5ff4b8007e167c31
lolguy91 <retek1544@gmail.com>
2024-05-19 10:25:49 +0000

    package.json parsing

--- src/package.zig
+++ src/package.zig
@@ -1,8 +1,19 @@
 const std = @import("std");
 
+pub const PkgJson = struct {
+    name: []const u8,
+    description: []const u8,
+    version: []const u8,
+    author: []const u8,
+    maintainer: []const u8,
+    license: []const u8,
+    pkg_type: []const u8,
+    url: []const u8
+};
+
 pub fn install(url: []const u8) !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Downloading the package...\n", .{});
+    try stdout.print("Downloading the package...\n\n", .{});
 
     const allocator = std.heap.page_allocator;
     const buffer = getBuffFromUrl(allocator, url) catch |e| {
@@ -11,7 +22,35 @@ pub fn install(url: []const u8) !void {
         return e; // this is here for the compiler
     };
     defer allocator.free(buffer);
-    std.debug.print("{s}\n", .{buffer});
+
+    const pkg_cont = std.json.parseFromSlice(PkgJson, allocator, buffer, .{.ignore_unknown_fields = true}) catch |e| {
+        std.debug.print("could not parse package.json: {s}\n", .{@errorName(e)});
+        std.process.exit(0xff);
+        return e; // this is here for the compiler
+    };
+    const pkg = pkg_cont.value;
+    defer pkg_cont.deinit();
+
+    try stdout.print("Package name: {s}\n", .{pkg.name});
+    try stdout.print("Description: {s}\n", .{pkg.description});
+    try stdout.print("Version: {s}\n", .{pkg.version});
+    try stdout.print("Author: {s}\n", .{pkg.author});
+    try stdout.print("Maintainer: {s}\n", .{pkg.maintainer});
+    try stdout.print("License: {s}\n\n", .{pkg.license});
+    try stdout.print("Install package? [y/n]: ", .{});
+    const confirm = try std.io.getStdIn().reader().readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
+    defer allocator.free(confirm.?);
+
+    if (!(std.mem.eql(u8, confirm.?, "y") or std.mem.eql(u8, confirm.?, "Y"))) {
+        std.process.exit(0xff);
+    }
+
+    try stdout.print("Installing package...\n", .{});
+
+
+
+    try stdout.print("Done!\n", .{});
+
 }
 
 pub fn getBuffFromUrl(allocator: std.mem.Allocator, url: []const u8) ![]u8 {
